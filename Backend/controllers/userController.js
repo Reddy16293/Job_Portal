da03bfda57348aa5b44a4fa28be659971c36b150
Reddy16293 <chilekampalli2004@gmail.com>
2024-07-26 16:19:36 +0530

    add login and logout contoller and routes in router

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -25,3 +25,39 @@ export const register = catchAsyncErrors(async (req, res, next) => {
 
   
 });
+
+export const login = catchAsyncErrors(async (req, res, next) => {
+    const { email, password, role } = req.body;
+    if (!email || !password || !role) {
+      return next(new ErrorHandler("Please provide email ,password and role."));
+    }
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return next(new ErrorHandler("Invalid Email Or Password.", 400));
+    }
+    const isPasswordMatched = await user.comparePassword(password);
+    if (!isPasswordMatched) {
+      return next(new ErrorHandler("Invalid Email Or Password.", 400));
+    }
+    if (user.role !== role) {
+      return next(
+        new ErrorHandler(`User with provided email and ${role} not found!`, 404)
+      );
+    }
+    sendToken(user, 201, res, "User Logged In!");
+  });
+
+  export const logout = catchAsyncErrors(async (req, res, next) => {
+    res
+      .status(201)
+      .cookie("token", "", {
+        httpOnly: true,
+        expires: new Date(Date.now()),
+      })
+      .json({
+        token:"",
+        success: true,
+        message: "Logged Out Successfully.",
+      });
+  });
+  
